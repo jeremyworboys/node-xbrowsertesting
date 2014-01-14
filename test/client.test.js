c1744dea4d15a7c9a719c10440f5eb37fd558b22
Jeremy Worboys <jw@jeremyworboys.com>
2014-01-14 20:56:32 +1100

    Add ability to make raw API calls

--- test/client.test.js
+++ test/client.test.js
@@ -5,6 +5,18 @@ var Client = require('../lib/Client');
 
 describe('Client', function() {
 
+    var config = {
+        username: 'username',
+        password: 'secretpassword'
+    };
+
+    var makeRequestStub = function(url) {
+        return function(options, done) {
+            expect(options.url).to.equal(url);
+            done();
+        };
+    };
+
     it('should throw an error when the config is missing', function() {
         var error;
         try { new Client(); }
@@ -21,6 +33,15 @@ describe('Client', function() {
 
         expect(error).to.exist;
         expect(error.message).to.contain('invalid');
+    });
+
+    it('should be able to make raw API calls', function(done) {
+        var client = new Client(config);
+        Client.request = makeRequestStub('http://example.com');
+
+        expect(client).to.respondTo('raw');
+
+        client.raw('http://example.com', done);
     });
 
 });
