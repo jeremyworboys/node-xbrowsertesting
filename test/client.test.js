8161b6d9b1faffc11315dcbda25a5c27e3357b50
Jeremy Worboys <jw@jeremyworboys.com>
2014-01-14 21:21:20 +1100

    Define API methods

--- test/client.test.js
+++ test/client.test.js
@@ -35,6 +35,79 @@ describe('Client', function() {
         expect(error.message).to.contain('invalid');
     });
 
+    it('should request the right url for client.listScreenshotBrowsers', function(done) {
+        var client = new Client(config);
+        Client.request = makeRequestStub('http://crossbrowsertesting.com/api/v2/screenshots/browsers');
+
+        expect(client).to.respondTo('listScreenshotBrowsers');
+
+        client.listScreenshotBrowsers(done);
+    });
+
+    it('should request the right url for client.runNewScreenshotTest', function(done) {
+        var client = new Client(config);
+        Client.request = makeRequestStub('http://crossbrowsertesting.com/api/v2/screenshots/run');
+
+        expect(client).to.respondTo('runNewScreenshotTest');
+
+        client.runNewScreenshotTest(done);
+    });
+
+    it('should request the right url for client.showScreenshotTests', function(done) {
+        var client = new Client(config);
+        Client.request = makeRequestStub('http://crossbrowsertesting.com/api/v2/screenshots/show');
+
+        expect(client).to.respondTo('showScreenshotTests');
+
+        client.showScreenshotTests(done);
+    });
+
+    it('should request the right url for client.showScreenshotTestVersions', function(done) {
+        var client = new Client(config);
+        Client.request = makeRequestStub('http://crossbrowsertesting.com/api/v2/screenshots/1111/show');
+
+        expect(client).to.respondTo('showScreenshotTestVersions');
+
+        client.showScreenshotTestVersions(1111, done);
+    });
+
+    it('should request the right url for client.showScreenshotVersionResults', function(done) {
+        var client = new Client(config);
+        Client.request = makeRequestStub('http://crossbrowsertesting.com/api/v2/screenshots/1111/version/2222/show');
+
+        expect(client).to.respondTo('showScreenshotVersionResults');
+
+        client.showScreenshotVersionResults(1111, 2222, done);
+    });
+
+    it('should request the right url for client.repeatScreenshotTest', function(done) {
+        var client = new Client(config);
+        Client.request = makeRequestStub('http://crossbrowsertesting.com/api/v2/screenshots/1111/version/2222/repeat');
+
+        expect(client).to.respondTo('repeatScreenshotTest');
+
+        client.repeatScreenshotTest(1111, 2222, done);
+    });
+
+    it('should request the right url for client.getScreenshotTestStatus', function(done) {
+        var client = new Client(config);
+        Client.request = makeRequestStub('http://crossbrowsertesting.com/api/v2/screenshots/1111/version/2222/status');
+
+        expect(client).to.respondTo('getScreenshotTestStatus');
+
+        client.getScreenshotTestStatus(1111, 2222, done);
+    });
+
+    it('should request the right url for client.retakeScreenshotTest', function(done) {
+        var client = new Client(config);
+        Client.request = makeRequestStub('http://crossbrowsertesting.com/api/v2/screenshots/result/3333/retake');
+
+        expect(client).to.respondTo('retakeScreenshotTest');
+
+        client.retakeScreenshotTest(3333, done);
+    });
+
+
     it('should be able to make raw API calls', function(done) {
         var client = new Client(config);
         Client.request = makeRequestStub('http://example.com');
